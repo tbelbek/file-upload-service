854d95da54a2d383ae160d5fbb716b3a452c6d35
tughan <Ttughanbelbek@gmail.com>
2019-02-12 14:41:50 +0300

    routing fix

--- FileUploader/App_Start/RouteConfig.cs
+++ FileUploader/App_Start/RouteConfig.cs
@@ -11,8 +11,6 @@ namespace FileUploader
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.Add(new SubdomainRoute());
-
             routes.MapRoute(
                     "FileLink",                                           // Route name
                     "FileLink/{fileId}",                            // URL with parameters
@@ -24,49 +22,6 @@ namespace FileUploader
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-        }
-    }
-
-    public class SubdomainRoute : RouteBase
-    {
-        public override RouteData GetRouteData(HttpContextBase httpContext)
-        {
-            if (httpContext.Request == null || httpContext.Request.Url == null)
-            {
-                return null;
-            }
-
-            var host = httpContext.Request.Url.Host;
-            var index = host.IndexOf(".");
-            string[] segments = httpContext.Request.Url.PathAndQuery.TrimStart('/').Split('/');
-
-            if (index < 0)
-            {
-                return null;
-            }
-
-            var subdomain = host.Substring(0, index);
-            string[] blacklist = { "www", "yourdomain", "mail" };
-
-            if (blacklist.Contains(subdomain))
-            {
-                return null;
-            }
-
-            string controller = (segments.Length > 0) ? segments[0] : "Home";
-            string action = (segments.Length > 1) ? segments[1] : "Index";
-
-            var routeData = new RouteData(this, new MvcRouteHandler());
-            routeData.Values.Add("controller", controller); //Goes to the relevant Controller  class
-            routeData.Values.Add("action", action); //Goes to the relevant action method on the specified Controller
-            routeData.Values.Add("subdomain", subdomain); //pass subdomain as argument to action method
-            return routeData;
-        }
-
-        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
-        {
-            //Implement your formating Url formating here
-            return null;
         }
     }
 }
